f692b2af1960fd0d1b9a44e34b8d7532e2ca5ed6
Sam Popuri <sspopuri@hotmail.com>
2025-06-27 00:06:28 -0500

    Final backend code deployment

---
+++ config/allowedOrigins.js
@@ -0,0 +1,8 @@
+const allowedOrigins = [
+    // 'http://localhost:3000',
+    // 'https://www.sparsinc.com',
+    // 'https://sparsinc.com'
+    'https://ticketingsystem-zfc8.onrender.com/'
+]
+
+module.exports = allowedOrigins
